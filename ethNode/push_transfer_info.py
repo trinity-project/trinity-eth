d2cbe9b1f0f05123c5dd4bbb83b52e83faa4e56d
maiganne <1730105653@qq.com>
2018-06-02 18:44:39 +0800

    modify

--- ethNode/push_transfer_info.py
+++ ethNode/push_transfer_info.py
@@ -35,7 +35,7 @@ class Erc20Tx(Base):
     address_to = Column(String(64),index=True)
     value = Column(Numeric(16,8))
     block_timestamp=Column(Integer)
-    hash_pushed=Column(Boolean,default=False)
+    has_pushed=Column(Boolean,default=False)
 
 
     def save(self):
