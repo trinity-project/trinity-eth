acaaf7f87ebda2b4f8aaaa44850c36af3c47c8d1
maiganne <1730105653@qq.com>
2018-07-17 15:11:23 +0800

    modify

--- monitor_event.py
+++ monitor_event.py
@@ -1,28 +1,84 @@
-import asyncio
+import json
+import time
+import redis
 from web3 import Web3, HTTPProvider
 
-w3 = Web3(HTTPProvider('http://192.168.214.178:8545'))
 
 
-def handle_event(event):
-    print(event)
-    # and whatever
+# pool= redis.ConnectionPool(host="localhost",port=6379,decode_responses=True)
+pool= redis.ConnectionPool(host="47.104.81.20",port=9001,decode_responses=True)
+redis_client=redis.Redis(connection_pool=pool)
 
-async def log_loop(event_filter, poll_interval):
+event_set=set()
+has_pushed=[]
+
+TOPICS_OF_ERC_TRANSFER="0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+ADDRESS_OF_ERC721_WOB="0x5296aAF564a7a15DE46840AA2b9742C205b0A70E"
+ADDRESS_OF_ERC20_TNC="0x65096f2b7a8dc1592479f1911cd2b98dae4d2218"
+
+
+
+def push_event(to_push_message):
     while True:
-        for event in event_filter.get_new_entries():
-            handle_event(event)
-        await asyncio.sleep(poll_interval)
+        try:
+            redis_client.publish("monitor", to_push_message)
+            return True
+        except Exception as e:
+            print("connect redis fail lead to push fail:{}".format(to_push_message))
+            time.sleep(3)
+
+
+
+def handle_logs(logs):
+
+    tmp_dict={}
+    for item in logs:
+        # print(item)
+        if item.get("address") ==ADDRESS_OF_ERC721_WOB:
+            if item.get("topics")[0].hex()==TOPICS_OF_ERC_TRANSFER:
+
+                tmp_dict["txId"]=item.get("transactionHash").hex()
+                tmp_dict["eventType"]="transfer"
+                tmp_dict["addressFrom"]="0x"+item.get("data")[26:66]
+                tmp_dict["addressTo"]="0x"+item.get("data")[90:130]
+                tmp_dict["messageType"]="monitorEthTransfer"
+                tmp_dict["tokenId"]=int("0x"+item.get("data")[130:194],16)
+
+                to_push_event=json.dumps(tmp_dict)
+                if to_push_event not in has_pushed:
+
+                    result=push_event(to_push_event)
+                    if result:
+                        has_pushed.append(to_push_event)
+
+
+
+
+def get_logs(web3):
+    event_logs = web3.eth.getLogs({
+        "fromBlock": "latest",
+        "address": [ADDRESS_OF_ERC721_WOB],
+        "topics": [TOPICS_OF_ERC_TRANSFER]
+    })
+
+    return event_logs
+
+def log_loop(web3, poll_interval=0):
+    while True:
+        event_logs = get_logs(web3)
+        handle_logs(event_logs)
+        time.sleep(poll_interval)
+
+        # break
+
+
+
 
 def main():
-    block_filter = w3.eth.filter({"address":"0x445E08fCE1E43606f8D39B14E372eA084B75C5f1"})
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(
-            asyncio.gather(
-                log_loop(block_filter, 2)))
-    finally:
-        loop.close()
+    # w3 = Web3(HTTPProvider('http://192.168.214.178:8545'))
+    # w3 = Web3(HTTPProvider('https://mainnet.infura.io/pZc5ZTRYM8wYfRPtoQal'))
+    w3 = Web3(HTTPProvider('https://ropsten.infura.io/pZc5ZTRYM8wYfRPtoQal'))
+    log_loop(w3)
 
 if __name__ == '__main__':
     main()
--- test_contract.py
+++ test_contract.py
@@ -7,24 +7,30 @@ import requests
 from eth_account.datastructures import AttributeDict
 from eth_utils import keccak
 from py_ecc.secp256k1 import privtopub
-from solc import compile_source, compile_files
+# from solc import compile_source, compile_files
 from ethereum import utils
 from ethereum.utils import ecsign, ecrecover_to_pub, privtoaddr, big_endian_to_int, normalize_key, int_to_big_endian, \
-    safe_ord, check_checksum, checksum_encode
+    safe_ord, check_checksum, checksum_encode, sha3, encode_int32
 from web3 import Web3, HTTPProvider
 import rlp
 from ethereum.transactions import Transaction
 
 # w3 = Web3(HTTPProvider('http://192.168.28.139:8545'))
-# w3 = Web3(HTTPProvider('http://192.168.28.139:8545'))
+w3 = Web3(HTTPProvider('http://192.168.214.178:8545'))
 
-# method=binascii.hexlify( w3.sha3(text="Mike(address)"))
+method=binascii.hexlify( w3.sha3(text="Test(address,address,uint256,uint256,address)"))
+# method=binascii.hexlify( w3.sha3(text="Transfer(address,address,uint256)"))
+print("method:",method)
+k="b6a03207128827eaae0d31d97a7a6243de31f2baf99eabd764e33389ecf436fc"
+k = normalize_key(k)
+print(k)
+x, y = privtopub(k)
+print(x,y)
+print(encode_int32(x))
+addr=sha3(encode_int32(x) + encode_int32(y))[12:]
 
 
-
-
-
-
+print(addr)
 
 def sendTransaction(addressFrom,addressTo,value,privtKey,data=b'',startgas=2560000):
 
