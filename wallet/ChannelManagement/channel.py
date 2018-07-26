64314c4cb22068a5e60eebb9180cbddf55b674a2
xiaohuta <hengjian30@163.com>
2018-07-26 15:45:13 +0800

    DEV: change some property function to static method

--- wallet/ChannelManagement/channel.py
+++ wallet/ChannelManagement/channel.py
@@ -227,9 +227,14 @@ class Channel(object):
     def query_trade(channel_name, nonce, *args, **kwargs):
         return APITransaction('trade'+channel_name).query_transaction(nonce, *args, **kwargs)
 
-    @property
-    def latest_trade(self):
-        return APITransaction('trade' + self.channel_name).sort(key='nonce')
+    @staticmethod
+    def latest_trade(self, channel_name):
+        trade = APITransaction('trade' + self.channel_name).sort(key='nonce')
+        if not trade:
+            LOG.error('No transaction records were found.')
+            return None
+
+        return trade
 
     # transaction related
     def transfer(self, sender, receiver, asset_type, count, hash_random=None, wallet=None):
@@ -243,14 +248,14 @@ class Channel(object):
         :param wallet:
         :return:
         """
-        # get nonce in the offline account book
-        if not not self.latest_trade:
-            LOG.error('No transaction records were found.')
-            return
-        latest_nonce = self.latest_trade.nonce
-
         channel = self.get_channel(sender, receiver, EnumChannelState.OPENED.name)
         if channel:
+            trade = Channel.latest_trade(channel.channel)
+            # get nonce in the offline account book
+            if not trade:
+                return
+            latest_nonce = trade.nonce
+
             # RSMC transaction
             mg.RsmcMessage.create(channel.channel, wallet, sender, receiver, count, int(latest_nonce)+1,
                                   asset_type, comments=hash_random)
