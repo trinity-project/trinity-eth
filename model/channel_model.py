7cd51060df0b4e810826931450ae5704e9fbf2f8
xiaohuta <hengjian30@163.com>
2018-08-20 17:28:54 +0800

    DEV: channel magic error

--- model/channel_model.py
+++ model/channel_model.py
@@ -34,7 +34,7 @@ class TBLChannel(DBManager):
         Modified        : 2018-03-21
     """
     def add_one(self, channel: str, src_addr: str, dest_addr: str, state: str, alive_block: int,
-                deposit:dict, magic:str, balance={}):
+                deposit:dict, magic:str, balance={}, **kwargs):
         """
 
         :param channel:
@@ -58,7 +58,7 @@ class TBLChannel(DBManager):
 
         return super(TBLChannel, self).add(channel=channel, src_addr=src_addr, dest_addr=dest_addr,
                                            state=state, alive_block=alive_block,
-                                           deposit=deposit, balance = deposit)
+                                           deposit=deposit, balance=deposit, magic=magic, **kwargs)
 
     def remove_unsupported_asset(self, asset):
         if not asset:
